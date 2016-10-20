24f10f5e02f995fc055cb1911a1a2fcf675d892b
Michal <michal.zeg@gmail.com>
2016-10-20 19:07:35 +0200

    Added Geometry & FEM menu item

---
+++ StruCal/Controllers/SectionPropertiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StruCal.Controllers
+{
+    public class SectionPropertiesController : Controller
+    {
+        // GET: SectionProperties
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
