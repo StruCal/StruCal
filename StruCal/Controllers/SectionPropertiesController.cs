83bcfae680ba6c3a3faccd734b96557f0ab389e9
Michal <michal.zeg@gmail.com>
2016-10-21 20:18:49 +0200

    Aded SectionProperties View and Controller

--- StruCal/Controllers/SectionPropertiesController.cs
+++ StruCal/Controllers/SectionPropertiesController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace StruCal.Controllers
 {
+    public class SectionPropertiesViewModel
+    {
+        private const string validationPattern = @"([1-9][0-9]+\.?[1-9]*;?)*";
+
+        [Display(Name ="X:")]
+        [Required]
+        [RegularExpression(validationPattern)]
+        public string XCoordinates { get; set; }
+
+        [Display(Name = "Y:")]
+        [Required]
+        [RegularExpression(validationPattern)]
+        public string YCoordinates { get; set; }
+    }
+
     public class SectionPropertiesController : Controller
     {
         // GET: SectionProperties
         public ActionResult Index()
         {
-            return View();
+            return View(new SectionPropertiesViewModel());
         }
     }
 }
