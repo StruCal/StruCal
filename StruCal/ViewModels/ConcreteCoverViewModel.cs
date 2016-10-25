0ec0ef2fdd3e78970ce9fc61a42f119ae3ce2a5a
kisala.dawid <kisala.dawid@gmail.com>
2016-10-25 18:37:31 +0200

    Add ConcreteCoverController, Improver ConcreteCoverView

--- StruCal/ViewModels/ConcreteCoverViewModel.cs
+++ StruCal/ViewModels/ConcreteCoverViewModel.cs
@@ -17,22 +17,36 @@ namespace StruCal.ViewModels
         public ConcreteCoverViewModel()
         {
             RebarDiameterSelectList = GetRebarDiameters();
+            // Select 16 mm rebar as default
+            RebarDiameterSelectList[7].Selected = true;
+
             ArrangementOfBarsSelectList = Enum.GetNames(typeof(ArrangementOfBars)).Select(name => new SelectListItem()
             {
                 Text = name,
                 Value = name
-            });
+            }).ToList();
+            // Select Separated arragement as default
+            ArrangementOfBarsSelectList[0].Selected = true;
+
             ConcreteClassSelectList = Enum.GetNames(typeof(ConcreteClass)).Select(name => new SelectListItem()
             {
                 Text = ConvertConcreteClassToString(name),
                 Value = ConvertConcreteClassToString(name)
-            });
+            }).ToList();
+            // Select C30/37 as default class
+            ConcreteClassSelectList[4].Selected = true;
+
             ExposureClassSelectList = Enum.GetNames(typeof(ExposureClass)).Select(name => new SelectListItem()
             {
                 Text = name,
                 Value = name
-            });
+            }).ToList();
+            // Select X0 as default class
+            ExposureClassSelectList[0].Selected = true;
+
             BaseStructuralClassSelectList = GetBaseStructuralClass();
+            // Select S4 class as default
+            BaseStructuralClassSelectList[3].Selected = true;
         }
 
         /// <summary>
@@ -43,38 +57,33 @@ namespace StruCal.ViewModels
         // Rebars properties
         [Required]
         [Display(Name = "Rebars:")]
-        public IEnumerable<SelectListItem> RebarDiameterSelectList { get; set; }
-        public SelectListItem SelectedRebarDiameter { get; set; }
+        public List<SelectListItem> RebarDiameterSelectList { get; set; }
+        public string SelectedRebarDiameter { get; set; }
 
         [Required]
         [Display(Name = "Arrangement of bars:")]
-        public IEnumerable<SelectListItem> ArrangementOfBarsSelectList { get; set; }
-        public SelectListItem SelectedArrangementOfBars { get; set; }
+        public List<SelectListItem> ArrangementOfBarsSelectList { get; set; }
+        public string SelectedArrangementOfBars { get; set; }
 
         // Concrete properties
         [Required]
         [Display(Name = "Concrete class:")]
-        public IEnumerable<SelectListItem> ConcreteClassSelectList { get; set; }
-        public SelectListItem SelectedConcreteClass { get; set; }
+        public List<SelectListItem> ConcreteClassSelectList { get; set; }
+        public string SelectedConcreteClass { get; set; }
 
         [Required]
         [Display(Name = "Exposure class:")]
-        public IEnumerable<SelectListItem> ExposureClassSelectList { get; set; }
-        public SelectListItem SelectedExposureClass { get; set; }
+        public List<SelectListItem> ExposureClassSelectList { get; set; }
+        public string SelectedExposureClass { get; set; }
 
         [Required]
         [Display(Name = "Base structural class:")]
-        public IEnumerable<SelectListItem> BaseStructuralClassSelectList { get; set; }
-        public SelectListItem SelectedBaseStructuralClass { get; set; }
+        public List<SelectListItem> BaseStructuralClassSelectList { get; set; }
+        public string SelectedBaseStructuralClass { get; set; }
 
         [Required]
         [Display(Name = "Is nominal maximum aggregate size is greater than 32mm?")]
-        public string NominalMaximumAggregateSizeIsGreaterThan32mm { get; set; }
-
-        // Bond Requirement
-        [Required]
-        [Display(Name = "Minimum cover due to bond requirement:")]
-        public string MinimumCoverDueToBondRequirement { get; set; }
+        public bool NominalMaximumAggregateSizeIsGreaterThan32mm { get; set; }
 
         // EnvironmentalConditions
         [Required]
@@ -89,9 +98,6 @@ namespace StruCal.ViewModels
         [Required]
         [Display(Name = "Structural class:")]
         public string StructuralClass { get; set; }
-        [Required]
-        [Display(Name = "Minimum cover due to environmental conditions:")]
-        public double MinimumCoverDueToEnvironmentalConditions { get; set; }
 
         // Additive Safety Element
         [Required]
@@ -109,50 +115,103 @@ namespace StruCal.ViewModels
         public double ReductionOfMinimumCoverForUseOfAdditionalProtection { get; set; }
 
         // Results
+        // Bond Requirement
+        [Required]
+        [Display(Name = "Minimum cover due to bond requirement:")]
+        public double MinimumCoverDueToBondRequirement { get; set; }
+
+        [Required]
+        [Display(Name = "Minimum cover due to environmental conditions:")]
+        public double MinimumCoverDueToEnvironmentalConditions { get; set; }
+
         [Required]
         [Display(Name = "Minimum cover:")]
         public double MinimumCover { get; set; }
 
         [Required]
         [Display(Name = "Allowance in design for deviation:")]
-        [RegularExpression(validationPattern)]
         public double AllowanceInDesignForDeviation { get; set; }
 
         [Required]
-        [Display(Name = "Nominal cover:")]
+        [Display(Name = "Nominal cover")]
         public double NominalCover { get; set; }
 
+        #region Converters
+        /// <summary>
+        /// Converts rebar diameter as String to double.
+        /// </summary>
+        /// <param name="rebarString"></param>
+        /// <returns></returns>
+        public double ConvertStringToRebarDiameter(string rebarString)
+        {
+            int index = rebarString.IndexOf(" ");
+            if (index > 0) rebarString = rebarString.Substring(0, index);
+
+            return double.Parse(rebarString) / 1000d;
+        }
+
+        /// <summary>
+        /// Converts arrangement of bars as String to enum.
+        /// </summary>
+        /// <param name="arrangementOfBarsString"></param>
+        /// <returns></returns>
+        public ArrangementOfBars ConvertStringToArrangementOfBars(string arrangementOfBarsString)
+        {
+            return (ArrangementOfBars)Enum.Parse(typeof(ArrangementOfBars), arrangementOfBarsString);
+        }
+
+        /// <summary>
+        /// Converts '_' to '/' for visual purposes of concrete class DropdownList.
+        /// </summary>
+        /// <param name="concreteClass"></param>
+        /// <returns></returns>
+        public string ConvertConcreteClassToString(string concreteClass)
+        {
+            return concreteClass.ToString().Replace("_", "/");
+        }
+
+        /// <summary>
+        /// Converts '_' to '/' for visual purposes of concrete class DropdownList.
+        /// </summary>
+        /// <param name="concreteClass"></param>
+        /// <returns></returns>
+        public string ConvertConcreteClassToString(ConcreteClass concreteClass)
+        {
+            return concreteClass.ToString().Replace("_", "/");
+        }
+
+        /// <summary>
+        /// Converts concrete class as String to Enum.
+        /// </summary>
+        /// <param name="concreteClass"></param>
+        /// <returns></returns>
+        public ConcreteClass ConvertStringToConcreteClass(string concreteClassString)
+        {
+            return (ConcreteClass)Enum.Parse(typeof(ConcreteClass), concreteClassString.Replace("/", "_"));
+        }
+
+        /// <summary>
+        /// Converts exposure class as String to Enum.
+        /// </summary>
+        /// <param name="exposureClass"></param>
+        /// <returns></returns>
+        public ExposureClass ConvertStringToExposureClass(string exposureClassString)
+        {
+            return (ExposureClass)Enum.Parse(typeof(ExposureClass), exposureClassString);
+        }
+
+        /// <summary>
+        /// Converts structural class as String to Enum.
+        /// </summary>
+        /// <param name="structuralClass"></param>
+        /// <returns></returns>
+        public StructuralClass ConvertStringToStructuralClass(string structuralClassString)
+        {
+            return (StructuralClass)Enum.Parse(typeof(StructuralClass), structuralClassString);
+        }
+        #endregion Converters
+
         #region Private Methods
-        /// <summary>
-        /// Converts '_' to '/' for visual purposes of concrete class DropdownList.
-        /// </summary>
-        /// <param name="concreteClass"></param>
-        /// <returns></returns>
-        private string ConvertConcreteClassToString(string concreteClass)
-        {
-            return concreteClass.ToString().Replace("_", "/");
-        }
-
-        /// <summary>
-        /// Converts '_' to '/' for visual purposes of concrete class DropdownList.
-        /// </summary>
-        /// <param name="concreteClass"></param>
-        /// <returns></returns>
-        private string ConvertConcreteClassToString(ConcreteClass concreteClass)
-        {
-            return concreteClass.ToString().Replace("_", "/");
-        }
-
-        /// <summary>
-        /// Converts '_' to '/' for visual purposes of concrete class DropdownList.
-        /// </summary>
-        /// <param name="concreteClass"></param>
-        /// <returns></returns>
-        private ConcreteClass ConvertStringToConcreteClass(string concreteClassString)
-        {
-            return (ConcreteClass)Enum.Parse(typeof(ConcreteClass), concreteClassString);
-        }
-
         /// <summary>
         /// List of available rebars diameters.
         /// </summary>
@@ -165,7 +224,7 @@ namespace StruCal.ViewModels
         /// Return all available rebars diameters as SelectListItems.
         /// </summary>
         /// <returns></returns>
-        private IEnumerable<SelectListItem> GetRebarDiameters()
+        private List<SelectListItem> GetRebarDiameters()
         {
             List<SelectListItem> rebarDiametersSelectList = new List<SelectListItem>();
 
@@ -182,19 +241,11 @@ namespace StruCal.ViewModels
             return rebarDiametersSelectList;
         }
 
-        public double ConvertStringToRebarDiameter(string rebarString)
-        {
-            int index = rebarString.IndexOf(" ");
-            if (index > 0) rebarString = rebarString.Substring(0, index);
-
-            return double.Parse(rebarString);
-        }
-
         /// <summary>
         /// Return all appropriate structural classes.
         /// </summary>
         /// <returns></returns>
-        private IEnumerable<SelectListItem> GetBaseStructuralClass()
+        private List<SelectListItem> GetBaseStructuralClass()
         {
             List<SelectListItem> baseStructuralClassSelectList = new List<SelectListItem>();
 
