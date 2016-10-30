b236ef07288b0cb62f5614fe22df90d887d263e7
kisala.dawid <kisala.dawid@gmail.com>
2016-10-30 20:02:28 +0100

    Improve main page.

--- StruCal/App_Start/BundleConfig.cs
+++ StruCal/App_Start/BundleConfig.cs
@@ -21,15 +21,13 @@ namespace StruCal
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/custom.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/custom.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/userScripts").Include(
-                      ("~/Scripts/custom.js")));
         }
     }
 }
