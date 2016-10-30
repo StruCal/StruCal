4dd158865bd84866bb4e12ca4742167d03511988
kisala.dawid <kisala.dawid@gmail.com>
2016-10-30 21:36:19 +0100

    Improve ConcreteCover calculator

--- StruCal/Scripts/custom.js
+++ StruCal/Scripts/custom.js
@@ -4,4 +4,5 @@
     $(document).ready(function () {
         $('[data-toggle="tooltip"]').tooltip({ animation: true, delay: { show: 200, hide: 50 }, selector: "", placement: "auto left", container: "body" });
     });
+
 })(jQuery); // End of use strict
