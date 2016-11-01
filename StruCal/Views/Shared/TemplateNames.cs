ade894e2e4cda33d444159346b9e80301de4fbbf
Michal <michal.zeg@gmail.com>
2016-11-01 21:19:29 +0100

    added tables with results to Section Properties

---
+++ StruCal/Views/Shared/TemplateNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StruCal.Views.Shared
+{
+    public static class TemplateNames
+    {
+        public static string SectionPropertyTableTemplate = "_SectionPropertyTableTemplate";
+    }
+}
