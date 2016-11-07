882f3fda145932dbb43a9bb93ef90785329e7406
kisala.dawid <kisala.dawid@gmail.com>
2016-11-07 21:20:11 +0100

    Add draggable toolbar (widget) to Concrete Cover View.

--- StruCal/Scripts/custom.js
+++ StruCal/Scripts/custom.js
@@ -2,7 +2,6 @@
     "use strict"; // Start of use strict
 
     $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip({ animation: true, delay: { show: 200, hide: 50 }, selector: "", placement: "auto left", container: "body" });
+        $('[data-toggle="tooltip"]').tooltip({ animation: true, delay: { show: 400, hide: 50 }, selector: "", placement: "auto left", container: "body" });
     });
-
 })(jQuery); // End of use strict
