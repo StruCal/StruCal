ae7fabf30bcfb9ec917cb96ea9dc1938365caeec
Michal <michal.zeg@gmail.com>
2016-12-02 22:08:00 +0100

    shear reinforcement calcs

--- Calculators/ShearReinforcement/ShearReinforcementCalculator.cs
+++ Calculators/ShearReinforcement/ShearReinforcementCalculator.cs
@@ -20,6 +20,14 @@ namespace Calculators.ShearReinforcement
         public double Asl { get; set; }
         public double h { get; set; }
         public double k1 { get; set; }
+
+        public double cotThetaMax { get; set; }
+        public double cotThetaMin { get; set; }
+        public double fywk { get; set; }
+        public double gammaS { get; set; }
+        public double Asw { get; set; }
+        public double s { get; set; }
+
     }
     public class ShearReinforcementOutput
     {
@@ -32,6 +40,17 @@ namespace Calculators.ShearReinforcement
         public double fcd { get; set; }
         public double ro1 { get; set; }
         public double k { get; set; }
+
+        public double theta { get; set; }
+        public double fywd { get; set; }
+        public double cotTheta { get; set; }
+        public double tanTheta { get; set; }
+        public double z { get; set; }
+        public double v1 { get; set; }
+        public double alfaCw { get; set; }
+        public double Vrds { get; set; }
+        public double Vrdmax { get; set; }
+        public bool ChangeSection { get; set; }
     }
 
     public class ShearReinforcementCalculator
@@ -44,11 +63,36 @@ namespace Calculators.ShearReinforcement
             var ro1 = inputData.Asl / (inputData.bw * inputData.d);
             var sigmacp = inputData.Ned / (inputData.h * inputData.bw);
             var Crdc = 0.18 / inputData.gammaC;
-            var vmin = 0.035 * Math.Pow(k, 2 / 3) * Math.Sqrt(inputData.fck);
+            var vmin = 0.035 * Math.Pow(k, 3 / 2) * Math.Sqrt(inputData.fck);
             var VRdc1 = (Crdc * k * Math.Pow(100 * ro1 * inputData.fck, 1 / 3) + k1 * sigmacp) * inputData.bw * inputData.d;
             var VRdc2 = (vmin + k1 * sigmacp) * inputData.bw * inputData.d;
             var Vrdc = Math.Max(VRdc1, VRdc2);
             var fcd = inputData.fck / inputData.gammaC;
+
+
+            var v1 = 0.6 * (1 - inputData.fck / 250);
+            var z = 0.9 * inputData.d;
+            var alfaCw = getAlfaCw(fcd, sigmacp);
+            var fywd = inputData.fywk / inputData.gammaS;
+            var theta = 0.5 * Math.Asin((2 * inputData.Ved) / (alfaCw * inputData.bw * z * v1 * fcd));
+            var changeSection = false;
+            var cotTheta = 0.0;
+            var tanTheta = 0.0;
+            var Vrdmax = 0.0;
+            var Vrds = 0.0;
+            if (theta.IsNaN())
+            {
+                //concrete strut fails
+                changeSection = true;
+            }
+            else
+            {
+                changeSection = false;
+                tanTheta = Math.Tan(theta);
+                cotTheta = Math.Pow(tanTheta, -1);
+                Vrdmax = alfaCw * inputData.bw * z * v1 * fcd / (tanTheta + cotTheta);
+                Vrds = (inputData.Asw / inputData.s) * z * fywd * cotTheta;
+            }
 
             var shearReinforcement = new ShearReinforcementOutput
             {
@@ -61,9 +105,33 @@ namespace Calculators.ShearReinforcement
                 Vrdc2 = VRdc2.Round(),
                 Vrdc = Vrdc.Round(),
                 fcd = fcd.Round(),
+
+                v1= v1.Round(),
+                z= z.Round(),
+                alfaCw=alfaCw.Round(),
+                fywd= fywd.Round(),
+                cotTheta= cotTheta.Round(),
+                tanTheta=tanTheta.Round(),
+                ChangeSection= changeSection,
+                theta= theta.Round(),
+                Vrdmax= Vrdmax.Round(),
+                Vrds= Vrds.Round(),
             };
 
             return shearReinforcement;
+        }
+
+        private double getAlfaCw(double fcd,double sigmaCp)
+        {
+            var result = 0.0;
+            if (sigmaCp <= 0.25 * fcd)
+                result = 1 + sigmaCp / fcd;
+            else if (0.25 * fcd < sigmaCp && sigmaCp <= 0.5 * fcd)
+                result = 1.25;
+            else if (0.5 * fcd < sigmaCp && sigmaCp <= fcd)
+                result = 2.5 * (1 - sigmaCp / fcd);
+
+            return result;
         }
     }
 }
