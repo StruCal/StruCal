7c4001c9c5f813888ca04feb94cf29719d1a9fb3
kisala.dawid <kisala.dawid@gmail.com>
2016-12-25 11:24:08 +0100

    Add binding between chart and input.

--- StruCal/Scripts/rcBeam.js
+++ StruCal/Scripts/rcBeam.js
@@ -10,26 +10,58 @@ var inputDataViewModel = function () {
     var self = this;
 
     //input parameters
-    self.RebarDiameterList = ko.observableArray(['4 mm', '5 mm', '6 mm', '8 mm', '10 mm', '12 mm', '14 mm', '16 mm', '18 mm', '20 mm', '22 mm', '25 mm', '28 mm', '32 mm', '40 mm']);
-    self.RebarDiameter = ko.observable('16 mm');
-    self.RebarDiameterValidation = ko.pureComputed(function () {
-        return self.RebarDiameter();
-    });
-
-    self.ArrangementOfBarsList = ko.observableArray(['Separated']);
-    self.ArrangementOfBars = ko.observable(0);
-    self.ArrangementOfBarsValidation = ko.pureComputed(function () {
-        return self.ArrangementOfBars();
-    });
-
     self.ConcreteClassList = ko.observableArray(['C12/15', 'C16/20', 'C20/25', 'C25/30', 'C30/37', 'C35/45', 'C40/50', 'C45/55', 'C50/60', 'C55/67', 'C60/75', 'C70/80', 'C80/95', 'C90/105']);
     self.ConcreteClass = ko.observable('C30/37');
     self.ConcreteClassValidation = ko.pureComputed(function () {
         return self.ConcreteClass();
     });
 
+    self.Fck = ko.observable(37);
+
     self.AlphaCC = ko.observable('1');
     self.AlphaCCValidation = ko.pureComputed(function () {
         return self.AlphaCC();
     });
+
+    this.RedValue = ko.observable(300);
+    this.GreenValue = ko.observable(50);
+    this.YellowValue = ko.observable(100);
+    this.StressStrain = {
+        labels: ["Red", "Green", "Yellow"],
+        datasets: [
+            {
+                data: [self.Fck, this.GreenValue, this.YellowValue],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }]
+    };
 }
+
+/**
+ * Vertically center Bootstrap 3 modals so they aren't always stuck at the top
+ */
+$(function () {
+    function reposition() {
+        var modal = $(this),
+            dialog = modal.find('.modal-dialog');
+        modal.css('display', 'block');
+
+        // Dividing by two centers the modal exactly, but dividing by three
+        // or four works better for larger screens.
+        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+    }
+    // Reposition when a modal is shown
+    $('.modal').on('show.bs.modal', reposition);
+    // Reposition when the window is resized
+    $(window).on('resize', function () {
+        $('.modal:visible').each(reposition);
+    });
+});
