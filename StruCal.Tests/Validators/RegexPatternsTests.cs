095a81b9c6ace2efb8d2d40af8d8bc2dd2804dc5
Michal <michal.zeg@gmail.com>
2016-12-27 16:23:07 +0100

    Section properties regex change and regex unit test update

--- StruCal.Tests/Validators/RegexPatternsTests.cs
+++ StruCal.Tests/Validators/RegexPatternsTests.cs
@@ -13,14 +13,14 @@ namespace StruCal.Tests.Validators
     public class RegexPatternsTests
     {
         [TestCase("10;20;30")]
-        [TestCase("00;20;30")]
-        [TestCase("10;20;30.")]
-        [TestCase("i10;20;30;")]
-        [TestCase("10;20k;30;")]
-        [TestCase("10;20;;30;")]
-        [TestCase("nm10;20;30")]
-        [TestCase("10,5;20,8;30")]
-        [TestCase("+10;20;30.2.3;")]
+        [TestCase("00;20 30;20.5 -20;18.2")]
+        [TestCase("10;20 30.;20 -;8")]
+        [TestCase("i10;20 30;20")]
+        [TestCase("10;20 30;20;")]
+        [TestCase("10;20;; 30;")]
+        [TestCase("10;20 20;5")]
+        [TestCase("10,5; 20,8 ;30")]
+        [TestCase("+10;20 30.2.3;")]
         public void RegexPatternTests_CustomSectionValidationPattern_ValidationShowErrors(string inputData)
         {
             var regex = new Regex(RegexPatterns.CustomSectionValidationPattern);
@@ -29,11 +29,11 @@ namespace StruCal.Tests.Validators
             Assert.IsFalse(result.Success);
         }
 
-        [TestCase("10;20;30;")]
-        [TestCase("-10;20;-30;")]
-        [TestCase("1.1;2.2;-5.02;")]
-        [TestCase("0.0;2;-30;")]
-        [TestCase("4;2.2;5;10;15;")]
+        [TestCase("10;20 30;40 50;60")]
+        [TestCase("-10;20 -30;-50 -80;100.2")]
+        [TestCase("1.1;2.2 -5.02;-4.02")]
+        [TestCase("0.0;2 -30;-0.02")]
+        [TestCase("4;2.2 5;10 15;-0.002")]
         public void RegexPatternTests_CustomSectionValidationPattern_ValidationPassed(string inputData)
         {
             var regex = new Regex(RegexPatterns.CustomSectionValidationPattern);
--- StruCal/Validators/RegexPatterns.cs
+++ StruCal/Validators/RegexPatterns.cs
@@ -7,7 +7,7 @@ namespace StruCal.Validators
 {
     public class RegexPatterns
     {
-        public const string CustomSectionValidationPattern = @"^((((-?)(0|[1-9][0-9]*)(\.[0-9]+)?);)+?)$";
+        public const string CustomSectionValidationPattern = @"^(((-?)(0|[1-9][0-9]*)(\.[0-9]+)?);((-?)(0|[1-9][0-9]*)(\.[0-9]+)?)\s)*(((-?)(0|[1-9][0-9]*)(\.[0-9]+)?);((-?)(0|[1-9][0-9]*)(\.[0-9]+)?))$";
         public const string PositiveNumberValidationPattern = @"^((0|[1-9][0-9]*)(\.[0-9]+)?)$";
     }
 }
