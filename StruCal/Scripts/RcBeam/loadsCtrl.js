891fccb6824751f5d94f463f9e1ebb20df9c58c4
Michal <michal.zeg@gmail.com>
2017-02-11 22:35:47 +0100

    load modal part1

---
+++ StruCal/Scripts/RcBeam/loadsCtrl.js
@@ -0,0 +1,28 @@
+﻿angular.module('rcBeam').controller('loadsCtrl', function ($scope, $rootScope) {
+    $scope.loadCases = new Array();
+
+    $scope.loadCases = [{
+        name: "load1",
+        value: 1000,
+    },
+    {
+        name: "load2",
+        value: 2000,
+    }];
+
+    $scope.save = function(index,name,value)
+    {
+        $scope.loadCases[index] = {
+            name : name,
+            value:value
+        }
+    }
+    $scope.add = function () {
+        $scope.loadCases.push({
+            name: "load",
+            value: 0
+        });
+    }
+
+
+});
