dd9783ae36f8770362286118c3ca6d8d8df5050b
Michal <michal.zeg@gmail.com>
2017-08-23 20:57:30 +0200

    geometry view and controller

--- StruCal/Scripts/Membrane/Controllers/geometryCtrl.js
+++ StruCal/Scripts/Membrane/Controllers/geometryCtrl.js
@@ -1,3 +1,36 @@
-﻿//angular.module('membraneFEM').controller('geometryCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+﻿angular.module('membraneFEM').controller('geometryCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $scope.vertices = new Array();
 
-//}]);
+    $scope.vertices = [{
+        x: 2000,
+        y: 3000,
+        supportX: true,
+        supportY: false,
+        loadX: 1000,
+        loadY:-500,
+    },
+    {
+        x: 5000,
+        y: 3000,
+        supportX: true,
+        supportY: false,
+        loadX: 5000,
+        loadY: -500,
+    }];
+
+    $scope.add = function () {
+        $scope.vertices.push({
+            x: 2000,
+            y: 3000,
+            supportX: true,
+            supportY: false,
+            loadX: 1000,
+            loadY: -500,
+        });
+    }
+
+    $scope.remove = function (index) {
+        $scope.vertices.splice(index, 1);
+    }
+
+}]);
