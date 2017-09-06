e2e433b161b870aad515b150d72c6e4e76ad6ebe
Michal <michal.zeg@gmail.com>
2017-09-06 18:53:01 +0200

    Section properties->decimal separator fix

--- Calculators/SectionProperties/Calculator.cs
+++ Calculators/SectionProperties/Calculator.cs
@@ -363,8 +363,8 @@ namespace Calculators.SectionProperties
 
             for (int i = 0; i < x.Length; i++)
             {
-                var xTemp = double.Parse(x[i].Trim(), CultureInfo.InvariantCulture);
-                var yTemp = double.Parse(y[i].Trim(), CultureInfo.InvariantCulture);
+                var xTemp = double.Parse(x[i].Trim());
+                var yTemp = double.Parse(y[i].Trim());
 
                 var point = new PointD(xTemp, yTemp);
 
