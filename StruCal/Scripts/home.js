d2fe04e09068e72549214e8ef3059c4931f1ccd3
Michal <michal.zeg@gmail.com>
2017-09-12 21:04:21 +0200

    changed main page

--- StruCal/Scripts/home.js
+++ StruCal/Scripts/home.js
@@ -1,5 +1,17 @@
 ﻿$('.img-small').hover(makeBigger, returnToOriginalSize);
 
+$('#cog').on('mouseover', function () {
+    var $this = $(this);
+
+    if ($(this).hasClass("gly-spin"))
+        return;
+    $(this).addClass("gly-spin");
+    setTimeout(function () {
+        $this.removeClass("gly-spin");
+    }, 2000);
+
+});
+
 function makeBigger() {
     $(this).css({ height: '+=10%', width: '+=10%' });
 }
