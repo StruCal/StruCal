43311fe684aaf4c74cafed2d57f4ca789e49996e
Michal <michal.zeg@gmail.com>
2018-01-07 16:26:01 +0100

    ts lint fix canvas helper

--- StruCal/Scripts/TrainLoad/src/app/canvasHelper/canvasHelper.ts
+++ StruCal/Scripts/TrainLoad/src/app/canvasHelper/canvasHelper.ts
@@ -1,28 +1,28 @@
-import { ElementRef } from "@angular/core";
+import { ElementRef } from '@angular/core';
 
-const widthHeightRatio : number = 3;
+const widthHeightRatio = 3;
 
-export class CanvasHelper{
+export class CanvasHelper {
 
-private _width :number;
-private _height:number;
+private _width: number;
+private _height: number;
 
-    constructor(canvas : HTMLElement){
-        canvas.setAttribute("style", "height:" + (canvas.offsetWidth / widthHeightRatio).toFixed(0) + "px");
+    constructor(canvas: HTMLElement) {
+        canvas.setAttribute('style', 'height:' + (canvas.offsetWidth / widthHeightRatio).toFixed(0) + 'px');
 
         this._width = canvas.clientWidth;
         this._height = canvas.clientHeight;
     }
 
-    get widthHeightRatio() : number{
+    get widthHeightRatio(): number{
         return widthHeightRatio;
     }
 
-    get width():number{
+    get width(): number{
         return this._width;
     }
 
-    get height():number{
+    get height(): number{
         return this._height;
     }
 }
