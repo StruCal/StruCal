1a0a628677c7b8bfd40e81ef506008dfcd7dbc3f
Michal <michal.zeg@gmail.com>
2018-01-12 21:39:03 +0100

    added train load binding model

--- StruCal/Controllers/TrainLoadController.cs
+++ StruCal/Controllers/TrainLoadController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using StruCal.BindingModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace StruCal.Controllers
@@ -12,6 +14,17 @@ namespace StruCal.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+    }
+
+    public class TrainLoadApiController : ApiController
+    {
+        [System.Web.Http.AllowAnonymous]
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult RcCalculations(TrainLoadBindingModel input)
+        {
+            
+            return Ok("OK");
         }
     }
 }
