3cbc4fe29ac63bba6b566d76feba4547d8957fd8
Michal <michal.zeg@gmail.com>
2018-01-14 19:19:14 +0100

    added train load calculator

--- Calculators/TrainLoad/Input/TrainLoadInput.cs
+++ Calculators/TrainLoad/Input/TrainLoadInput.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace StruCal.BindingModels
+namespace Calculators.TrainLoad
 {
     public class TrainLoadInput
     {
---
+++ Calculators/TrainLoad/TrainLoadCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators.TrainLoad
+{
+    public class TrainLoadCalculator
+    {
+        private readonly TrainLoadInput trainLoadInput;
+        private readonly IGradientCalculator gradientCalculator;
+
+        public TrainLoadCalculator(TrainLoadInput trainLoadInput)
+        {
+            this.trainLoadInput = trainLoadInput;
+            this.gradientCalculator = new LinearGradientCalculator(this.trainLoadInput.MaxColor, this.trainLoadInput.MinColor);
+
+        }
+
+
+    }
+}
