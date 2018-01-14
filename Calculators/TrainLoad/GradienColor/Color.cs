8200c645d04d846ec8ccc4a10b71feef3620447c
Michal <michal.zeg@gmail.com>
2018-01-14 19:12:51 +0100

    GradientColor calculator added

---
+++ Calculators/TrainLoad/GradienColor/Color.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators.TrainLoad.GradienColor
+{
+    internal class Color
+    {
+        const int hex = 16;
+
+        public int R { get; private set;}
+        public int G { get; private set;}
+        public int B { get; private set; }
+
+        private Color()
+        {
+           
+        }
+
+        public string GetHexCode()
+        {
+            var r = this.R.ToString("X");
+            var g = this.G.ToString("X");
+            var b = this.B.ToString("X");
+            var result = r + g + b;
+            return result;
+        }
+
+        public string GetHexCodeHased()
+        {
+            return "#" + this.GetHexCode();
+        }
+
+        public static Color FromRGB(int r, int g, int b)
+        {
+            var color = new Color();
+            color.R = r;
+            color.G = g;
+            color.B = b;
+            return color;
+        }
+
+        public static Color FromHexString(string colorNumber)
+        {
+            var color = new Color();
+            var colorTrimmed = colorNumber.Trim('#');
+            color.R = Convert.ToInt32(colorTrimmed.Substring(0, 2), hex);
+            color.G = Convert.ToInt32(colorTrimmed.Substring(2, 2), hex);
+            color.B = Convert.ToInt32(colorTrimmed.Substring(4, 2), hex);
+            return color;
+        }
+    }
+}
---
+++ Calculators/TrainLoad/GradienColor/GradientCalculator.cs
@@ -0,0 +1,35 @@
+﻿using Calculators.TrainLoad.GradienColor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators.TrainLoad
+{
+    
+
+    internal class GradientCalculator
+    {
+        private readonly Color maxColor;
+        private readonly Color minColor;
+        private readonly int range;
+
+        public GradientCalculator(string maxColor, string minColor, int range = 10)
+        {
+            this.maxColor = Color.FromHexString(maxColor);
+            this.minColor = Color.FromHexString(minColor);
+            this.range = range;
+        }
+
+
+        public Color ColorAt(int index)
+        {
+            var rAverage = this.minColor.R + (int)((this.maxColor.R - this.minColor.R) * index / this.range);
+            var gAverage = this.minColor.G + (int)((this.maxColor.G - this.minColor.G) * index / this.range);
+            var bAverage = this.minColor.B + (int)((this.maxColor.B - this.minColor.B) * index / this.range);
+            var result = Color.FromRGB(rAverage, gAverage, bAverage);
+            return result;
+        }
+    }
+}
