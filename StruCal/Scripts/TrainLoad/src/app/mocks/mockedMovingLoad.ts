75b796e23bf7398294e497df48e88a8e99c1cfc4
Michal <michal.zeg@gmail.com>
2018-01-17 19:26:11 +0100

    added moving Load

---
+++ StruCal/Scripts/TrainLoad/src/app/mocks/mockedMovingLoad.ts
@@ -0,0 +1,21 @@
+import { MovingLoad } from '../movingLoad/movingLoad';
+
+
+
+export const mockedMovingLoad: MovingLoad = {
+    speed: 1,
+    forces: [
+        {
+            basePosition: -1,
+            load: -1000,
+        },
+        {
+            basePosition: -2,
+            load: -1000,
+        },
+        {
+            basePosition: -3,
+            load: -1000,
+        },
+    ]
+};
---
+++ StruCal/Scripts/TrainLoad/src/app/movingLoad/movingLoad.ts
@@ -0,0 +1,9 @@
+import { MovingForce } from './movingForce';
+
+
+
+export class MovingLoad {
+    speed: number;
+    forces: Array<MovingForce>;
+
+}
