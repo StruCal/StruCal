0758f3f811590bccf0b8e713c77659f936499254
Michal <michal.zeg@gmail.com>
2018-01-21 21:40:10 +0100

    implemented modal base window

--- StruCal/Scripts/TrainLoad/src/app/modal-section1/modal-section1.component.ts
+++ StruCal/Scripts/TrainLoad/src/app/modal-section1/modal-section1.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ModalBaseComponent } from '../modal-base/modal-base.component';
+import { ModalBase } from '../modal-base/modalBase';
 
 @Component({
   selector: 'modal-section1',
   templateUrl: './modal-section1.component.html',
   styleUrls: ['./modal-section1.component.css']
 })
-export class ModalSection1Component implements OnInit {
+export class ModalSection1Component implements OnInit, ModalBase {
 
+  @ViewChild(ModalBaseComponent)
+  private modalBase: ModalBaseComponent;
   constructor() { }
+
+  show(): void {
+    this.modalBase.show();
+  }
+  hide(): void {
+    this.modalBase.hide();
+  }
 
   ngOnInit() {
   }
