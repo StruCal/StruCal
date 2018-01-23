218fe145b51e16d3c0f08eaa0c3df54286e2199e
Michal <michal.zeg@gmail.com>
2018-01-23 19:58:39 +0100

    added hsmlBBuilder

---
+++ StruCal/Scripts/TrainLoad/src/common/movingLoad/trainLoadBuilders/HSLMBBuilder.ts
@@ -0,0 +1,34 @@
+import { MovingLoad } from '../movingLoad';
+import { MovingForce } from '../movingForce';
+
+
+export function HSMLBBuilder() {
+    const trainLoad = new MovingLoad();
+    const pointLoad = 170;
+    let numberOfForces: number;
+    let distanceBetweenForces: number;
+    const movingForces = new Array<MovingForce>();
+    return { setSpeed };
+    function setSpeed(speed: number) {
+        trainLoad.speed = speed;
+        return { setNumberOfForces };
+    }
+    function setNumberOfForces(n: number) {
+        numberOfForces = n;
+        return { setDistanceBetweenForces };
+    }
+    function setDistanceBetweenForces(d: number) {
+        distanceBetweenForces = d;
+        return { build };
+    }
+    function build(): MovingLoad {
+        const forces = Array.from(Array(numberOfForces).keys())
+            .map(e => distanceBetweenForces * e);
+        forces.forEach(position => {
+            movingForces.push({ basePosition: position, load: pointLoad });
+        });
+
+        trainLoad.forces = movingForces;
+        return trainLoad;
+    }
+}
