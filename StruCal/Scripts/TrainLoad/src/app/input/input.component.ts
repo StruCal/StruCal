8364a273b433ea376131d289347ef5930c7df876
Michal <michal.zeg@gmail.com>
2018-01-26 19:21:09 +0100

    created input component

--- StruCal/Scripts/TrainLoad/src/app/input/input.component.ts
+++ StruCal/Scripts/TrainLoad/src/app/input/input.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core/src/metadata/directives';
+import { Input } from '@angular/core';
+import { section1Input } from '../modal-section1/Input/section1/section1Input';
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'input',
+  selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
 
+  @Input() inputs;
+  @Output() change = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChange() {
+    this.change.emit();
   }
 
 }
