b97659158b9b0559b9a9ab749dceec3a33435307
Michal <michal.zeg@gmail.com>
2018-02-06 21:38:36 +0100

    train load added to control panel

--- StruCal/Scripts/TrainLoad/src/app/services/control-panel.service.ts
+++ StruCal/Scripts/TrainLoad/src/app/services/control-panel.service.ts
@@ -35,6 +35,7 @@ export class ControlPanelService {
 
     this.structureService.trainLoad$.subscribe(e => {
       this.movingLoad = e;
+      this.setStructure();
       this.statusBarService.setDirty();
     });
 
