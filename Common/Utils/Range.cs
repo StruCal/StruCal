cdb651edf0206645df95f8afe87e3bed2591e758
Michal <michal.zeg@gmail.com>
2018-02-07 20:41:28 +0100

    added Range Object to Common

---
+++ Common/Utils/Range.cs
@@ -0,0 +1,23 @@
+﻿using Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Utils
+{
+    public static class Range
+    {
+        public static IEnumerable<double> GetRange(double startTime, double endTime, double step)
+        {
+            var time = startTime;
+            while (time < endTime || time.IsApproximatelyEqualTo(endTime))
+            {
+                yield return time;
+                time += step;
+            }
+
+        }
+    }
+}
