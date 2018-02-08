54df1ce1a159604c8cdecce2ddad945ad1f9ddd0
Michal <michal.zeg@gmail.com>
2018-02-08 21:28:12 +0100

    amended wrong name of class

--- StruCal/Scripts/TrainLoad/src/3DDrawing/model3d/resultsCreator/stressProvider.ts
+++ StruCal/Scripts/TrainLoad/src/3DDrawing/model3d/resultsCreator/stressProvider.ts
@@ -6,12 +6,12 @@ import { StructureData } from '../structureCreator/structureData';
 export class StressProvider {
     private structureData: StructureData;
     private scene: THREE.Scene;
-    private resultInterpolation: ResultProvider;
+    private resultProvider: ResultProvider;
     private meshes: Array<any>;
 
-    constructor(scene: THREE.Scene, resultInterpolation: ResultProvider, structureData: StructureData) {
+    constructor(scene: THREE.Scene, resultProvider: ResultProvider, structureData: StructureData) {
         this.scene = scene;
-        this.resultInterpolation = resultInterpolation;
+        this.resultProvider = resultProvider;
         this.structureData = structureData;
         this.meshes = this.scene.children.filter(e => e.type === 'Mesh' && structureData.isStructureMesh(e.uuid));
     }
@@ -33,7 +33,7 @@ export class StressProvider {
 
                 for (let i = 0; i < 3; i++) {
                     const vertex = faceVertices[i];
-                    const color = this.resultInterpolation.getColor(vertex, mesh.uuid);
+                    const color = this.resultProvider.getColor(vertex, mesh.uuid);
                     face.vertexColors[i].set(color);
 
                 }
