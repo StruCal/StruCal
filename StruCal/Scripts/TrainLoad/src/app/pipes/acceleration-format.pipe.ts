4f57be2680e853f41a788ee1f729b69c031e4386
Michal <michal.zeg@gmail.com>
2018-03-20 20:27:58 +0100

    pipe implementation with formating acceleration

--- StruCal/Scripts/TrainLoad/src/app/pipes/acceleration-format.pipe.ts
+++ StruCal/Scripts/TrainLoad/src/app/pipes/acceleration-format.pipe.ts
@@ -9,7 +9,12 @@ export class AccelerationFormatPipe implements PipeTransform {
     if (!value) {
       return null;
     }
-    return value * 10000;
+    const inversion = Math.round(Math.pow(value, -1));
+    const decimalPlaces = inversion.toString().length + 1;
+    const multiplier = Math.pow(10, decimalPlaces);
+
+    const newValue = Math.round(value * multiplier);
+    return newValue;
   }
 
 }
